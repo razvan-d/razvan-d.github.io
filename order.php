de6257c6877b24e190d407cbbe40c781c059e17f
mVali <44539925+MVali12@users.noreply.github.com>
2020-04-21 23:07:55 +0300

    php2

--- order.php
+++ order.php
@@ -1,8 +1,33 @@
 <?php
-$connect = mysql_connect(“MySQL”, “root”, “”); 
-if (!connect) { 
-    die('Connection Failed: ' . mysql_error()); 
- } 
- mysql_select_db(“comenzi”, $connect);
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname']; 
+$subject = $_POST['subject']
+$status = $_POST['status']
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "comenzi";
+
+$conn = new mysqli($servername, $username, $password, $dbname); 
+
+if (!$conn){
+die('The connection is not possible: ' . mysql_error());
+}
+
+mysql_select_db("comenzi", $conn);
+
+$sql="INSERT INTO comenzi(Status,FName,FName2,Subject) VALUES('".$status."','".$fname."','".$lname."''".$subject."')";
+
+echo 'Database selected';
+
+if (!mysql_db_query($conn, $sql))
+      {
+      die('Error: ' . mysql_error());
+      }
+    echo ('<b>Successful insertion!!!</b>');
+
+    mysql_close($conn);
+
 
 ?>
