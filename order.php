0d29bea4bae22582402b7355abde1e23ea8877c8
mVali <44539925+MVali12@users.noreply.github.com>
2020-04-22 00:08:27 +0300

    php

--- order.php
+++ order.php
@@ -4,20 +4,21 @@ $fname = $_POST['fname'];
 $lname = $_POST['lname']; 
 $subject = $_POST['subject']
 $status = $_POST['status']
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "root";
 $password = "";
 $dbname = "comenzi";
 
-$conn = new mysqli($servername, $username, $password, $dbname); 
+$conn = mysqli_connect($servername, $username, $password)or die("cannot connect"); 
+mysqli_select_db($conn, $dbname)or die("cannot select DB"); 
 
 if (!$conn){
 die('The connection is not possible: ' . mysql_error());
 }
 
-mysql_select_db("comenzi", $conn);
+mysql_select_db($dbname, $conn);
 
-$sql="INSERT INTO comenzi(Status,FName,FName2,Subject) VALUES('".$status."','".$fname."','".$lname."''".$subject."')";
+$sql="INSERT INTO 'comenzi'('Status','FName','FName2','Subject') VALUES ('$status','$fname','$lname','$subject')";
 
 echo 'Database selected';
 
